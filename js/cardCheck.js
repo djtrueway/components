a369e6453b58df696f0549d223c8e97eaa138145
djtrueway <saintaimepeterson@gmail.com>
2023-06-12 15:31:49 -0400

    control card auto on frontend

---
+++ js/cardCheck.js
@@ -0,0 +1,122 @@
+/*    JavaScript 
+ *    Filename: script1.js
+ */
+
+"use strict";
+
+function fadeIn(el) {
+    el.style.opacity = 0;
+    var tick = function () {
+        el.style.opacity = +el.style.opacity + 0.01;
+        if (+el.style.opacity < 1) {
+            (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+        }
+    };
+    tick();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+// FUNCTION - VALIDATE CREDIT CARD NUMBER (original code by DiegoSalazar)
+//////////////////////////////////////////////////////////////////////////////////////
+function validateCardNumber(value){
+
+    // ACCEPT ONLY DIGITS, DASHES OR SPACES
+    if(/[^0-9-\s]+/.test(value)){
+
+        return false;
+
+    }
+
+    // LUHN ALGORITHM
+    var nCheck = 0;
+    var nDigit = 0;
+    var bEven = false;
+
+    // REMOVE SPACES
+    value = value.replace(/\D/g,"");
+
+    // LOOP
+    for(var n = value.length - 1; n >= 0; n--){
+
+        // SET VALUES
+        var cDigit = value.charAt(n);
+        var nDigit = parseInt(cDigit, 10);
+
+        // CHECK FOR EVEN
+        if(bEven){
+
+            // CHECK FOR SOMETHING
+            if((nDigit *= 2) > 9){
+
+                // SUBSRACT 9
+                nDigit -= 9;
+
+            }
+
+        }
+
+        // INCREASE
+        nCheck += nDigit;
+        bEven = !bEven;
+
+    }
+
+    // RETURN
+    return (nCheck % 10) == 0;
+
+} // END FUNCTION
+
+function format(s) {
+    return s.toString().replace(/\d{4}(?=.)/g, '$& ');
+}
+
+var img = document.createElement("img");
+img.alt = "logo card";
+
+function selectCardType() {
+  // local variables
+    var cardNumValue = document.getElementById("card_input").value;
+    // var visa = /^4[0-9]{12}(?:[0-9]{3})?$/;
+    var visa = /^4[0-9]{15}$/;
+    var mc = /^5[1-5][0-9]{14}$/;
+    var discover = /^6(?:011|5[0-9]{2})[0-9{12}$/]/;
+    var amex = /^3[0-7][0-9]{14}$/;
+
+    if (visa.test(cardNumValue)) {
+        // add logo on input 
+        const img_input = document.getElementById("img_card")
+        img_input.removeChild(img_input.firstElementChild);
+        img.src = "https://e7.pngegg.com/pngimages/308/426/png-clipart-visa-logo-credit-card-visa-logo-payment-visa-blue-text-thumbnail.png"
+        img.width = 56
+        img.height = 32
+        img_input.appendChild(img);
+
+        document.getElementById("card_input").value = format(cardNumValue);
+        console.log(validateCardNumber(cardNumValue));
+
+    } else
+    if (mc.test(cardNumValue)) {
+        // add logo on input
+        const img_input = document.getElementById("img_card")
+        img_input.removeChild(img_input.firstElementChild);
+        img.src = "https://w7.pngwing.com/pngs/924/607/png-transparent-mastercard-credit-card-business-debit-card-logo-mastercard-text-service-orange.png";
+        img.width = 56
+        img.height = 32
+        img_input.appendChild(img);
+
+        console.log(validateCardNumber(cardNumValue));
+        document.getElementById("card_input").value = format(cardNumValue);
+      // document.getElementById("mc").checked = "checked";
+    } else if (discover.test(cardNumValue)) {
+        console.log(validateCardNumber(cardNumValue));
+        document.getElementById("card_input").value = format(cardNumValue);
+        alert("discover");
+      // document.getElementById("discover").checked = "checked";
+    } else if (amex.test(cardNumValue)) {
+        console.log(validateCardNumber(cardNumValue));
+        document.getElementById("card_input").value = format(cardNumValue);
+        alert("amex");
+      // document.getElementById("amex").checked = "checked";
+    }
+}
+
